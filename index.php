3efaf563a71d8dbf01afb8f2006da929e31dbd51
André Luiz Teodoro de Amorim <andre.amorim@mt.transportes.gov.br>
2018-10-30 08:49:24 -0300

    Commit inicial

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>ATM's Nutrition</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+
+<body>
+
+    <!-- Nav tabs -->
+    <ul class="nav nav-tabs|pills nav-top" id="navId">
+        <li class="nav-item nav-top-li">
+            <a href="index.php" class="nav-link active">Manter Dieta</a>
+        </li>
+        <li class="nav-item nav-top-li">
+            <a href="perfil.php" class="nav-link active">Perfil</a>
+        </li>
+        <li class="nav-item nav-top-li">
+            <a href="nutrientes.php" class="nav-link">Nutrientes</a>
+        </li>
+    </ul>
+
+</body>
+</html>
---
+++ nutrientes.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>ATM's Nutrition</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+
+    <!-- Nav tabs -->
+    <ul class="nav nav-tabs|pills nav-top" id="navId">
+        <li class="nav-item nav-top-li">
+            <a href="index.php" class="nav-link active">Manter Dieta</a>
+        </li>
+        <li class="nav-item nav-top-li">
+            <a href="perfil.php" class="nav-link active">Perfil</a>
+        </li>
+        <li class="nav-item nav-top-li">
+            <a href="nutrientes.php" class="nav-link">Nutrientes</a>
+        </li>
+    </ul>
+
+    <br><br>
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Nome</th>
+      <th scope="col">Característica</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+    </tr>
+  </tbody>
+</table>
+
+</body>
+</html>
